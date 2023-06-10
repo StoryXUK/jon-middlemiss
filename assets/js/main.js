cadcdc752dff82c848e354f409b35e60057fee67
Responsibleuk <james@we-are-responsible.com>
2023-06-10 15:22:24 +0000

    Add survey modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -945,3 +945,23 @@ $(document).on('click', '.shop-qty .plus, .shop-qty .minus', function(){
   }
 
   });
+
+  // Modal
+ 
+  // Get the modal element
+  var modal = document.getElementById('surveyModal');
+
+  // Get the button that opens the modal
+  var button = document.querySelector('.survey-button');
+
+  // When the button is clicked, open the modal
+  button.addEventListener('click', function() {
+    modal.style.display = 'block';
+  });
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.addEventListener('click', function(event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
